2a5254f82d6ae34230151918a29f46813f159117
biribin97 <biribin97g@gmail.com>
2025-11-02 01:19:53 -0300

    first commit

---
+++ CreatuLanding1+Biribin/src/App.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import "react";
+import "react-bootstrap/dist/react-bootstrap.min.js";
+import NavBar from './componentes/NavBar.jsx';
+
+
+function App() {
+  return (
+    <div>
+      <nav>
+      <li>inicio</li>
+      <li>icqn</li>
+      <li>ewgv</li>
+      </nav>
+<h1>hola</h1>
+    </div>
+  )
+}
+
+
+
+export default App
